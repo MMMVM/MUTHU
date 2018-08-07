431b202489c7ef1d8d67ea9b16f9fbcfe7f57757
MMMVM <42170207+MMMVM@users.noreply.github.com>
2018-08-07 14:55:05 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Positive
+{
+public static void main(String args[])
+{
+int n;
+Scanner n=new scanner(system.in)
+if(n>0)
+{
+system.out.println("the num is positive");
+}
+else if(n==0)
+{
+system.out.println("the num is zero");
+}
+else
+{
+system.out.println("the num is negative");
+}
+}
+}
